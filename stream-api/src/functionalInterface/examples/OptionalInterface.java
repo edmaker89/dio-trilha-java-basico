970cb456ae54cd954663152e1832377d11325b85
edmaker <edmaker@gmail.com>
2024-08-17 09:45:53 -0300

    Optional Interface

---
+++ stream-api/src/functionalInterface/examples/OptionalInterface.java
@@ -0,0 +1,10 @@
+package functionalInterface.examples;
+
+public class OptionalInterface {
+/**
+ * Usado para quando um valor pode ser nulo para evitar nullPointerException
+ */
+public static void main(String[] args) {
+    
+}
+}
